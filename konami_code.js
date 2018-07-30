2d3cc458503caa00d75b0c04d5a5046e75706e53
Doug Newman <dougnewman@gmail.com>
2018-07-30 18:56:19 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,35 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+console.log(code)
+
 function init() {
   // Write your JavaScript code inside the init() function
 
+  const body = document.querySelector("body")
+
+  let index = 0;
+
+  body.addEventListener("keydown",
+    function(e) {
+      console.log(e)
+      const key = parseInt(e.detail || e.which);
+     
+     console.log(code)
+     
+      if (key === code[index]) {
+        index++;
+      }
+      
+      if (index === code.length) {
+          alert("Hurray!");
+          index = 0;
+      }
+    }
+  )
+
+  console.log(body);
 }
+
+init()
+
+
