949eee8bc5157c6f6649baf27fcad5ab3c939c82
Symone Clarke <shaneilske@outlook.com>
2018-08-01 18:44:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+let index = 0;
+
+document.addEventListener('keydown', e => {
+  const key = parseInt(e.detail || e.which);
+  let correctKey = code[index];
+  if (key === correctKey) {
+    index++;
+    if (index === code.length) {
+      alert("Congrats! Cheat unlocked");
+      index = 0;
+    }
+  } else {
+    index = 0;
+    alert("Oops! keep trying");
+  }
+})
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  init();
+})
